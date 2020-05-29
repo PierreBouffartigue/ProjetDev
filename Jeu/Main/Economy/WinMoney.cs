98edcf398ec1c5dce1f52dbcdbbb966f2211b11b
thomasmlc <43410009+thomasmlc@users.noreply.github.com>
2020-05-29 16:09:34 +0200

    Update WinMoney.cs

--- Jeu/Main/Economy/WinMoney.cs
+++ Jeu/Main/Economy/WinMoney.cs
@@ -1,48 +1,61 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography;
 using System;
 
-public class WinMoney : MonoBehaviour {
-	public Button BoutonMoney;
-	[SerializeField] private Image imagepiece;
+public class WinMoney : MonoBehaviour
+{
+    public Button BoutonMoney;
+    [SerializeField] private Image imagepiece;
+    private float Countdown;
+    private int money;
+    private int moneyToWin;
 
-	private float Countdown;
-	private int money;
+    void Start()
+    {
+        //Temps avant d'ajouter de l'argent à la "cagnotte"
+        Countdown = 5;
+        //Argent gagné disponible lors du clic sur le bouton. "cagnotte"
+        money = 1;
+        //Récupere le BoutonMoney et le lie à une méthode
+        Button btn = BoutonMoney.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
 
-	private int moneyToWin;
+    void Update()
+    {
+        //Recuperation du nombre de pieces à gagner à la fin du timer
+        moneyToWin = upgradeMoney.moneyToWin;
+        Countdown -= Time.deltaTime;
+        //si Countdown passe sous 0
+        if (Countdown < 0)
+        {
+            // on le reinitialise
+            Countdown = 5;
+            //et on ajoute les pieces à gagner à la "cagnotte"
+            money += moneyToWin;
+        }
+        //Si 0 dans la "cagnotte"
+        if (money == 0)
+        {
+            //Le bouton pour récupérer l'argent n'apparaît pas
+            imagepiece.enabled = false;
+        }
+        else
+        {
+            //Sinon il apparaît
+            imagepiece.enabled = true;
+        }
+    }
 
-	void Start () {
-		Countdown = 5;
-		money = 1;
-		Button btn = BoutonMoney.GetComponent<Button>();
-		btn.onClick.AddListener(TaskOnClick);
-	}
-
-	void Update()
-	{
-		moneyToWin = upgradeMoney.moneyToWin;
-		Countdown -= Time.deltaTime;
-		if(Countdown < 0)
-		{
-			Countdown = 5;
-			money += moneyToWin;
-		}
-
-		if(money == 0)
-		{
-			imagepiece.enabled = false;
-		}
-		else
-		{
-			imagepiece.enabled = true;
-		}
-	}
-
-	void TaskOnClick(){
-		PlayerStats.IncreaseMoney(money);
-		money = 0;
-	}
+    //Tâche exécuté lors de l'appui sur le bouton
+    void TaskOnClick()
+    {
+        //Incrémente l'agent du joueur avec le nombre de pièces dans la "cagnotte"
+        PlayerStats.IncreaseMoney(money);
+        //Reinitialise la "cagnotte"
+        money = 0;
+    }
 }

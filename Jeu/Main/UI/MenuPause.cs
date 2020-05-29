c829160c1b07e22c577ebb1498c36aa699bd1827
Akiyoma <43410025+Akiyoma@users.noreply.github.com>
2020-05-29 16:33:05 +0200

    Update MenuPause.cs

--- Jeu/Main/UI/MenuPause.cs
+++ Jeu/Main/UI/MenuPause.cs
@@ -10,28 +10,39 @@ public class MenuPause : MonoBehaviour
     public GameObject pauseMenuUI;
 
     void Update()  {
+        // Si on appuie sur échap
         if (Input.GetKeyDown(KeyCode.Escape)) {
+            // Si le jeu est en pause
             if (GameIsPaused) {
+                // Reprendre le jeu
                 Resume();
             } else {
+                // Sinon le mettre en pause
                 Pause();
             }
         }
     }
 
     public void Resume (){
+        // Enlever l'affichage du menu pause
         pauseMenuUI.SetActive(false);
+        // Vitesse du jeu normale
         Time.timeScale = 1f;
+        // Définie le jeu comme n'étant pas en pause
         GameIsPaused = false;
     }
 
     void Pause () {
+        // Affiche le menu pause
         pauseMenuUI.SetActive(true);
+        // Vitesse du jeu à 0 (en pause)
         Time.timeScale = 0f;
+        // Définie le jeu comme étant en pause
         GameIsPaused = true;
     }
 
     public void LoadMenu(){
+        // Charge la scene de login
         SceneManager.LoadScene("Login");
     }
 

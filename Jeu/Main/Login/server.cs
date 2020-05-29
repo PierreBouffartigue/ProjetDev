4de4d6e583aacd51bd96259c8ed032c3d2c421e1
PierreBouffartigue <pierre.bouffartigue@ynov.com>
2020-05-29 21:18:13 +0200

    Update server.cs

--- Jeu/Main/Login/server.cs
+++ Jeu/Main/Login/server.cs
@@ -13,6 +13,7 @@ public class server : MonoBehaviour
 
 	WWWForm form;
 
+	//Quand le bouton est cliqué il devient indisponible le temps de la vérification 
 	public void OnLoginButtonClicked ()
 	{
 		loginButton.interactable = false;
@@ -22,21 +23,25 @@ public class server : MonoBehaviour
 	IEnumerator Login ()
 	{
 		form = new WWWForm ();
-
+		
+		//Envoi les identifiants au serveur web pour qu'il les valide
 		form.AddField ("username", username.text);
 		form.AddField ("password", password.text);
 
 		WWW w = new WWW (url, form);
 		yield return w;
 
+		//Si le serveur web est injoignable alors le message "Erreur" apparait
 		if (w.error != null) {
 			errorMessages.text = "Erreur";
 			Debug.Log("<color=red>"+w.text+"</color>");
 		} else {
 			if (w.isDone) {
+				//Si la liaison au serveur web est bonne et que les identifiants entrés sont invalides alors un message d'erreur apparait
 				if (w.text.Contains ("error")) {
 					errorMessages.text = "Identifiant invalide";
 					Debug.Log("<color=red>"+w.text+"</color>");
+				//Une fois l'authentification réussie, la première scène est chargée
 				} else {
 					SceneManager.LoadScene(1);
 					Debug.Log("<color=green>"+w.text+"</color>");

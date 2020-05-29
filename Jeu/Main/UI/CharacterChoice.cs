8091414b30e9c79ec3dbcb5781de6035b728026b
Akiyoma <43410025+Akiyoma@users.noreply.github.com>
2020-05-29 16:50:36 +0200

    Update CharacterChoice.cs

--- Jeu/Main/UI/CharacterChoice.cs
+++ Jeu/Main/UI/CharacterChoice.cs
@@ -4,46 +4,57 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class CharacterChoice : MonoBehaviour {
-    public GameObject ChoixPersoObject;
+    	public GameObject ChoixPersoObject;
 	public Button BoutonChoixHomme;
-    public Button BoutonChoixFemme;
+    	public Button BoutonChoixFemme;
 	[SerializeField] private Image iconHomme;
-    [SerializeField] private Image iconFemme;
-    [SerializeField] private Image choixHomme;
-    [SerializeField] private Image choixFemme;
+    	[SerializeField] private Image iconFemme;
+    	[SerializeField] private Image choixHomme;
+    	[SerializeField] private Image choixFemme;
 
 	void Start () {
+		// Affiche les 2 images de coix
 		choixFemme.enabled = true;
-        choixHomme.enabled = true;
-        iconHomme.enabled = false;
-        iconFemme.enabled = false;
+        	choixHomme.enabled = true;
+		// Cache les 2 icons
+        	iconHomme.enabled = false;
+        	iconFemme.enabled = false;
+		// Récuperation des boutons
 		Button btv = BoutonChoixHomme.GetComponent<Button>();
-        Button btc = BoutonChoixFemme.GetComponent<Button>();
+        	Button btc = BoutonChoixFemme.GetComponent<Button>();
+		// Ecoute les boutons
 		btc.onClick.AddListener(HommeBouton);
-        btv.onClick.AddListener(FemmeBouton);
+        	btv.onClick.AddListener(FemmeBouton);
 	}
 
+	// Quand le bouton homme est cliqué
 	void HommeBouton() {
-        choixFemme.enabled = false;
-        choixHomme.enabled = false;
-        iconHomme.enabled = false;
-        iconFemme.enabled = true;
-        Button btv = BoutonChoixHomme.GetComponent<Button>();
-        Button btc = BoutonChoixFemme.GetComponent<Button>();
-        btv.enabled = false;
-        btc.enabled = false;
-        ChoixPersoObject.SetActive(false);
+		// Enlève les images de choix
+		choixFemme.enabled = false;
+		choixHomme.enabled = false;
+		// Affiche l'icon homme
+		iconHomme.enabled = false;
+		iconFemme.enabled = true;
+		// Récupère les boutons pour les désactiver
+		Button btv = BoutonChoixHomme.GetComponent<Button>();
+		Button btc = BoutonChoixFemme.GetComponent<Button>();
+		btv.enabled = false;
+		btc.enabled = false;
+		ChoixPersoObject.SetActive(false);
 	}
-
-    void FemmeBouton() {
-        choixFemme.enabled = false;
-        choixHomme.enabled = false;
-        iconHomme.enabled = true;
-        iconFemme.enabled = false;
-        Button btv = BoutonChoixHomme.GetComponent<Button>();
-        Button btc = BoutonChoixFemme.GetComponent<Button>();
-        btv.enabled = false;
-        btc.enabled = false;
-        ChoixPersoObject.SetActive(false);
-    }
+	// Quand le bouton homme est cliqué
+    	void FemmeBouton() {
+		// Enlève les images de choix
+		choixFemme.enabled = false;
+		choixHomme.enabled = false;
+		// Affiche l'icon femme
+		iconHomme.enabled = true;
+		iconFemme.enabled = false;
+		// Récupère les boutons pour les désactiver
+		Button btv = BoutonChoixHomme.GetComponent<Button>();
+		Button btc = BoutonChoixFemme.GetComponent<Button>();
+		btv.enabled = false;
+		btc.enabled = false;
+		ChoixPersoObject.SetActive(false);
+    	}
 }

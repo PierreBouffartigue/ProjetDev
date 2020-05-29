0299b2d793c7103400feacbb3d881d86e94f48f6
Akiyoma <43410025+Akiyoma@users.noreply.github.com>
2020-05-29 16:35:54 +0200

    Update TailleEcran.cs

--- Jeu/Main/UI/TailleEcran.cs
+++ Jeu/Main/UI/TailleEcran.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class TailleEcran : MonoBehaviour {
     public void Reso()
     {
+        // Définie la résolution à 640x480 et en plein écran
         Screen.SetResolution(640, 480, true);
     }
 }

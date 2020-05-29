500dc08d21b43a63930c5285e1decc46adea5491
Akiyoma <43410025+Akiyoma@users.noreply.github.com>
2020-05-29 17:01:57 +0200

    Update TransportRenew.cs

--- Jeu/Main/Utilities/TransportRenew.cs
+++ Jeu/Main/Utilities/TransportRenew.cs
@@ -14,33 +14,41 @@ public class TransportRenew : MonoBehaviour
 
     void Start()
     {
+    	// Affiche le village du joueur et désactive le village ennemie et le champ de bataille
         VillagePrincipal.SetActive(true);
         VillageEnnemi.SetActive(false);
         ChampBataille.SetActive(false);
+	// Récupération et écoute des trois boutons de déplacement
         Button btt = TransportEnnemi.GetComponent<Button>();
-		btt.onClick.AddListener(Ennemi);
+	btt.onClick.AddListener(Ennemi);
         Button bty = TransportBataille.GetComponent<Button>();
-		bty.onClick.AddListener(Bataille);
+	bty.onClick.AddListener(Bataille);
         Button btu = TransportVillage.GetComponent<Button>();
-		btu.onClick.AddListener(Village);
+	btu.onClick.AddListener(Village);
     }
 
+    // Quand le boutin transportennemie est cliqué
     void Ennemi()
     {
+    	// Affiche le village ennemie et désactive les autres
         VillagePrincipal.SetActive(false);
         VillageEnnemi.SetActive(true);
         ChampBataille.SetActive(false);
     }
 
+    // Quand le boutin transportbataille est cliqué
     void Bataille()
     {
+    	// Affiche le champ de bataille et désactive les autres
         VillagePrincipal.SetActive(false);
         VillageEnnemi.SetActive(false);
         ChampBataille.SetActive(true);
     }
 
+    // Quand le boutin transportvillage est cliqué
     public void Village()
     {
+    	// Affiche le village du joueur et désactive les autres
         VillagePrincipal.SetActive(true);
         VillageEnnemi.SetActive(false);
         ChampBataille.SetActive(false);

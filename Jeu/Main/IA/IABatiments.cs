c33a26736d6c17a106dfa1aa48f166aefb4f1d44
thomasmlc <43410009+thomasmlc@users.noreply.github.com>
2020-05-29 17:31:32 +0200

    Update IABatiments.cs

--- Jeu/Main/IA/IABatiments.cs
+++ Jeu/Main/IA/IABatiments.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
@@ -11,19 +11,26 @@ public class IABatiments : MonoBehaviour
 
     void Start()
     {
+		//Desactive les différents batiments
         BatimentEnnemi1.SetActive(false);
         BatimentEnnemi2.SetActive(false);
         Forge.SetActive(false);
     }
 
     void Update(){
+		//si l'argent du joueur atteint 6 ou plus
         if (PlayerStats.money >= 6){
+			//fait apparaitre le premier batiment
             BatimentEnnemi1.SetActive(true);
         }
+		//si l'argent du joueur atteint 8 ou plus
         if(PlayerStats.money >= 8){
+			//fait apparaitre la forge
             Forge.SetActive(true);
         }
+		//si l'argent du joueur atteint 10 ou plus
         if(PlayerStats.money >= 10){
+			//fait apparaitre le deuxieme batiment
             BatimentEnnemi1.SetActive(true);
         }
     }

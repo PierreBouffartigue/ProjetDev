38f52e14f495a781e8b4ba265329aa93f127a7d1
thomasmlc <43410009+thomasmlc@users.noreply.github.com>
2020-05-29 16:20:38 +0200

    Update upgradeMoney.cs

--- Jeu/Main/Economy/upgradeMoney.cs
+++ Jeu/Main/Economy/upgradeMoney.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
@@ -7,34 +7,33 @@ using System;
 
 public class upgradeMoney : MonoBehaviour
 {
-	public GameObject moneyDisplay;
+    public GameObject moneyDisplay;
+    private PlayerStats moneyScipt;
+    public Button BoutonUpgrade;
+    [SerializeField] private Image imageupgrade;
+    public static int moneyToWin;
 
-	private PlayerStats moneyScipt;
-
-	public Button BoutonUpgrade;
-	[SerializeField] private Image imageupgrade;
-
-	
-	public static int moneyToWin;
-
-	void Start()
-	{
-		
-		moneyToWin = 1;
-		Button btn = BoutonUpgrade.GetComponent<Button>();
-		btn.onClick.AddListener(TaskOnClick);
-	}
-
-	void TaskOnClick()
-	{
-		
-		if (PlayerStats.money >= moneyToWin * 4)
+    void Start()
+    {
+        //Initialisation de l'argent à ajouter dans la "cagnotte" de l'hotel de ville
+        moneyToWin = 1;
+        //Récuperation du bouton d'amélioration et une methode lui est liée
+        Button btn = BoutonUpgrade.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+    //Quand click sur le bouton
+    void TaskOnClick()
+    {
+        //Si le joueur a exactement ou plus d'argent que 4 fois l'argent qui s'ajoute à la "cagnotte"
+        if (PlayerStats.money >= moneyToWin * 4)
         {
-			PlayerStats.money -= moneyToWin * 4;
-			moneyToWin += 1;
+            // il perd alors cette somme 
+            PlayerStats.money -= moneyToWin * 4;
+            //et l'argent qui s'ajoute à la cagnotte augmente de 1
+            moneyToWin += 1;
 
-		}
+        }
 
-		
-	}
+
+    }
 }

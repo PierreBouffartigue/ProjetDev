74acd3e31a7c9f601205186df43164798b50c5c8
Akiyoma <43410025+Akiyoma@users.noreply.github.com>
2020-05-29 16:38:01 +0200

    Update Options.cs

--- Jeu/Main/UI/Options.cs
+++ Jeu/Main/UI/Options.cs
@@ -4,7 +4,7 @@ public class Options : MonoBehaviour
 {
     public void Full()
     {
-
+        // Change la fenêtre de plein écran à fenêtré et inversement
         Screen.fullScreen = !Screen.fullScreen;
     }
 }
